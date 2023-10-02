e58cc2a9c5294376483e0b42230fcbe4092629f0
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:01:58 +0200

    test of SQL export

--- Concept/schema.sql
+++ Concept/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Concept of the subject dataModel.STAT-DCAT-AP for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Concept of the subject dataModel.STAT-DCAT-AP for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Concept_type AS ENUM ('Concept');
-CREATE TABLE Concept (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, inScheme text, language json, location json, name text, notation text, owner json, prefLabel json, seeAlso json, source text, type Concept_type);
+CREATE TABLE Concept (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, inScheme TEXT, language JSON, name TEXT, notation TEXT, owner JSON, prefLabel JSON, source TEXT, type Concept_type);
