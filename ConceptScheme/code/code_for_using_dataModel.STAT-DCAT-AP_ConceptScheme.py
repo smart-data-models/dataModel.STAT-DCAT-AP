ec8d05c207a5633d3137c9b9ab3aec01d35a58f8
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:41:28 +0200

    created example of code

--- ConceptScheme/code/code_for_using_dataModel.STAT-DCAT-AP_ConceptScheme.py
+++ ConceptScheme/code/code_for_using_dataModel.STAT-DCAT-AP_ConceptScheme.py
@@ -24,25 +24,30 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "ConceptScheme"
 subject = "dataModel.STAT-DCAT-AP"
-language = {'type': 'Property', 'value': ['en', 'fr']}
+language = ['en', 'fr']
 attribute = "language"
 value = language
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-hasTopConcept = {'type': 'Relationship', 'value': ['urn:ngsi-ld:Concept:C', 'urn:ngsi-ld:Concept:I', 'urn:ngsi-ld:Concept:K', 'urn:ngsi-ld:Concept:M', 'urn:ngsi-ld:Concept:N', 'urn:ngsi-ld:Concept:R', 'urn:ngsi-ld:Concept:S', 'urn:ngsi-ld:Concept:T', 'urn:ngsi-ld:Concept:W', 'urn:ngsi-ld:Concept:X', 'urn:ngsi-ld:Concept:Y', 'urn:ngsi-ld:Concept:_Z']}
+hasTopConcept = ['urn:ngsi-ld:Concept:C', 'urn:ngsi-ld:Concept:I', 'urn:ngsi-ld:Concept:K', 'urn:ngsi-ld:Concept:M', 'urn:ngsi-ld:Concept:N', 'urn:ngsi-ld:Concept:R', 'urn:ngsi-ld:Concept:S', 'urn:ngsi-ld:Concept:T', 'urn:ngsi-ld:Concept:W', 'urn:ngsi-ld:Concept:X', 'urn:ngsi-ld:Concept:Y', 'urn:ngsi-ld:Concept:_Z']
 attribute = "hasTopConcept"
 value = hasTopConcept
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-prefLabel = {'type': 'Property', 'value': {'en': 'Ajustement saisonnier', 'fr': 'Seasonal ajustments'}}
+prefLabel = {'en': 'Ajustement saisonnier', 'fr': 'Seasonal ajustments'}
 attribute = "prefLabel"
 value = prefLabel
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
