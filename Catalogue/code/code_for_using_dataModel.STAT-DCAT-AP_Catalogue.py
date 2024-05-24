70faaadfcd2a71b893c1aff8feb80d3ac0e8b4ad
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:41:21 +0200

    created example of code

--- Catalogue/code/code_for_using_dataModel.STAT-DCAT-AP_Catalogue.py
+++ Catalogue/code/code_for_using_dataModel.STAT-DCAT-AP_Catalogue.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Catalogue"
 subject = "dataModel.STAT-DCAT-AP"
-dataset = {'type': 'Relationship', 'object': ['urn:ngsi-ld:Catalogue:dataset:VLNR:72960176']}
+dataset = ['urn:ngsi-ld:Catalogue:dataset:VLNR:72960176']
 attribute = "dataset"
 value = dataset
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-publisher = "{'type': 'Property', 'value': 'INE'}"
+publisher = "INE"
 attribute = "publisher"
 value = publisher
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-title = {'type': 'Property', 'value': ['Catalogue or statistical resources', 'Catálogo de recursos estadisticos']}
+title = ['Catalogue or statistical resources', 'Catalogo de recursos estadisticos']
 attribute = "title"
 value = title
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-homepage = "{'type': 'Property', 'value': 'urn:ngsi-ld:Catalogue:homepage:FXWI:96370263'}"
+homepage = "urn:ngsi-ld:Catalogue:homepage:FXWI:96370263"
 attribute = "homepage"
 value = homepage
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
